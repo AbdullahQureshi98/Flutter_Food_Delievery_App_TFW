e2ff8634d6335d5a65dca98783cc0f95102908c5
xypherDev <abdullahqureshi4444@gmail.com>
2021-10-13 21:16:49 +0500

    Day_1

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kWhiteColor = Color(0xffFFFFFF);
+const kBlackColor = Color(0xff000000);
+const kTextColor = Color(0xff1D150B);
+const kPrimaryColor = Color(0xffFb475F);
+const kSecondaryColor = Color(0xffF5E1CB);
+const kBorderColor = Color(0xffDDDDDD);
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:food_delievery_app_tfw/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food App',
+      theme: ThemeData(
+        fontFamily: "Poppins",
+        scaffoldBackgroundColor: kWhiteColor,
+        primaryColor: kPrimaryColor,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          SvgPicture.asset('assets/icons/menu.svg'),
+        ],
+      ),
+    );
+  }
+}
